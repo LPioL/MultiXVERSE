00cf4d62f47a6e71c2cda2277d884ecba42797b8
LPioL <leo.pio.lopez@gmail.com>
2024-07-22 15:24:51 +0200

    files

---
+++ MultiXVERSE.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 11:09:00 2019
+
+@author: Léo Pio-Lopez
+"""
+
+import numpy as np
+import argparse
+import os
+import datetime
+import utils as f
+import pandas as pd
+import multiprocess
+
+import multixrank
+import pandas as pd
+import multiprocess as mp
+import yaml
+
+
+def main(args=None):
+        
+
+
+    ########################################################################
+    # Parameters multiverse and train/test
+    ########################################################################
+
+    EMBED_DIMENSION = 128
+    CLOSEST_NODES = np.int64(300)
+    NUM_SAMPLED = np.int64(10)
+    LEARNING_RATE = np.float64(0.01)
+    NB_CHUNK = np.int64(1)
+    CHUNK_SIZE = np.int64(100)
+    NUM_STEPS_1 = np.int64(100*10**6/CHUNK_SIZE)
+    
+
+    #If toy example
+    EMBED_DIMENSION = 128
+    CLOSEST_NODES = np.int64(2)
+    NUM_SAMPLED = np.int64(10)
+    LEARNING_RATE = np.float64(0.01)
+    NB_CHUNK = np.int64(1)
+    CHUNK_SIZE = np.int64(2)
+    NUM_STEPS_1 = np.int64(100*10**6/CHUNK_SIZE)
+    
+    
+    ##################################################################################
+    # !! Careful !! 
+    # Check if nodes in the bipartite have the same nodes in the multiplex
+    # networks. If not you have to remove the nodes in the multiplexes not included in the  
+    # bipartites
+    ##################################################################################
+    
+
+    ###################################################################################"
+    # MULTIXRANK
+    ###################################################################################"
+ 
+
+    num_cpu=4
+
+    multixrank_obj = multixrank.Multixrank(config="airport/config_minimal.yml", wdir="airport")
+    nodes_multiplex = multixrank_obj.multiplexall_node_list2d
+    # Using list comprehension to flatten the list of lists
+    nodes_multiplex = pd.DataFrame([(i, node) for i, nodes in enumerate(nodes_multiplex) for node in nodes], columns=['network', 'node'])
+
+    # Make all seeds.txt and associated config_minimal.yml
+    with open('airport/config_minimal.yml', 'r') as file:
+        file_param = yaml.safe_load(file)
+            
+    for i in  range(len(nodes_multiplex.node)):
+        with open('airport/configs/seeds'+str(i)+'.txt', 'w+') as file:
+            file.write(str(nodes_multiplex.node[i]))
+
+        file_param['seed'] = 'seeds'+str(i)+'.txt'
+        with open('airport/configs/config_minimal'+str(i)+'.yml', 'w+') as file:
+            document = yaml.dump(file_param, file)
+
+    def mxrank(k):
+        print(str(k) + '\n')
+        multixrank_obj = multixrank.Multixrank(config='airport/configs/config_minimal'+str(k)+'.yml', wdir="airport/configs")
+        print('he')
+        ranking_df = multixrank_obj.random_walk_rank()
+        output_obj = multixrank.Output(ranking_df, multixrank_obj, top=None)
+        res = output_obj._df
+        res.multiplex=multixrank_obj.seed_obj.seed_list*len(res)   
+        return res
+
+    list_similarities=[]
+    p = mp.Pool(processes=num_cpu)
+    list_similarities.append(p.map(mxrank, [i for i in range(len(nodes_multiplex.node))])    )
+
+    similarity_matrix_list = pd.concat(list_similarities[0], axis=0, ignore_index=False)
+    similarity_matrix = similarity_matrix_list.pivot(index='multiplex', columns='node', values='score')
+
+
+        ########################################################################
+        # Processing of the network
+        ########################################################################
+        
+    reverse_data_DistancematrixPPI, list_neighbours, nodes, rawdata_DistancematrixPPI, neighborhood, nodesstr = f.netpreprocess1(similarity_matrix, CLOSEST_NODES)
+     
+        ########################################################################
+        # Initialization
+        ######################################################################## 
+
+    embeddings = np.random.normal(0, 1, [np.size(nodes), EMBED_DIMENSION])
+ 
+        ########################################################################
+        # Training and saving best embeddings   
+        ######################################################################## 
+    # Train and test during training
+    neighborhood = np.asarray(neighborhood)
+    nodes= np.asarray(nodes)
+    
+    embeddings = f.train(neighborhood, nodes, list_neighbours, NUM_STEPS_1, NUM_SAMPLED, LEARNING_RATE, \
+                         CLOSEST_NODES, CHUNK_SIZE, NB_CHUNK, embeddings, reverse_data_DistancematrixPPI)
+ 
+    X = dict(zip(range(embeddings.shape[0]), embeddings))
+    X = {str(int(nodesstr[key])+1): X[key] for key in X}
+    np.save('embeddings_MH',X)
+    os.replace('embeddings_MH.npy', './results_Embeddings/'+ 'embeddings_MH.npy')
+    
+if __name__ == "__main__":
+    main()
+    
+    
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 08:20:28 2019
+
+@author: Léo Pio-Lopez
+"""
+
+import numpy as np
+import scipy as sc
+from numba import jit, njit, prange
+import math
+from numpy.random import choice
+from sklearn.preprocessing import normalize
+import pandas as pd
+import networkx as nx
+from evalne.utils import preprocess as pp
+
+@njit
+def rand_choice_nb(arr, prob):
+    """
+    :param arr: A 1D numpy array of values to sample from.
+    :param prob: A 1D numpy array of probabilities for the given samples.
+    :return: A random sample from the given array with a given probability.
+    """
+    return arr[np.searchsorted(np.cumsum(prob), np.random.random(), side="right")]
+
+@njit
+def node_positive_weighted (u, list_neighbours, CLOSEST_NODES, reverse_data_DistancematrixPPI):
+    if np.sum(reverse_data_DistancematrixPPI[u])==0:
+        return int(list_neighbours[u][np.random.randint(1,CLOSEST_NODES)])
+    else:
+       probas = reverse_data_DistancematrixPPI[u,0:CLOSEST_NODES]
+       draw = rand_choice_nb(list_neighbours[u,0:CLOSEST_NODES], probas)
+       return int(draw)
+            
+@njit
+def sigmoid(x): 
+    return 1 / (1 + math.exp(-x))
+
+def node_negative (u, list_neighbours, CLOSEST_NODES): 
+    return int(list_neighbours[u][np.random.randint(np.size(list_neighbours[0])-CLOSEST_NODES,np.size(list_neighbours[0]))])
+
+@njit 
+def update (W_u, W_v, D, learning_rate, bias):
+    sim = sigmoid(np.dot(W_u,W_v)-bias)
+    gradient = (D - sim)*learning_rate
+    W_u = W_u + gradient * W_v
+    W_v = W_v + gradient * W_u
+    return W_u, W_v, gradient
+
+@njit(parallel=True)
+def train(neighborhood, nodes, list_neighbours, NUM_STEPS, NUM_SAMPLED, LEARNING_RATE, CLOSEST_NODES, CHUNK_SIZE, NB_CHUNK, embeddings, reverse_data_DistancematrixPPI):
+    
+    nb_nodes=np.int64(np.shape(nodes)[0])
+    # NCE biases
+    nce_bias = np.float64(np.log(nb_nodes))
+    nce_bias_neg = np.float64(np.log(nb_nodes/NUM_SAMPLED))
+          
+    for k in prange (NUM_STEPS):
+        nodes_opt= np.random.randint(0, np.int64(nb_nodes),CHUNK_SIZE)   
+        if k % int(10**6/CHUNK_SIZE) == 0:
+            print (k)
+            
+        for i in prange (CHUNK_SIZE):
+            u = nodes_opt[i]
+            v = node_positive_weighted (u, list_neighbours, CLOSEST_NODES, reverse_data_DistancematrixPPI)
+            embeddings[u,:], embeddings[v,:], gradientpos = update (embeddings[u,:],  embeddings[v,:], 1, LEARNING_RATE, nce_bias)
+
+            for j in range (NUM_SAMPLED):
+                v_neg =  list_neighbours[u, np.random.randint(CLOSEST_NODES+1,nb_nodes-1)]        
+                embeddings[u,:], embeddings[v_neg,:], gradientneg = update (embeddings[u,:],  embeddings[v_neg,:], 0, LEARNING_RATE, nce_bias_neg)
+
+    return embeddings
+
+def knbrs(G, start, k):
+    nbrs = set([start])
+    for l in range(k):
+        nbrs = set((nbr for n in nbrs for nbr in G[n]))
+    return nbrs
+     
+# from EvalNE  
+def preprocess(inpath, outpath, delimiter, directed, relabel, del_self_loops):
+    """
+    Graph preprocessing routine.
+    """
+    print('Preprocessing graph...')
+
+    # Load a graph
+    G = pp.load_graph(inpath, delimiter=delimiter, comments='#', directed=directed)
+
+    # Preprocess the graph
+    G, ids = pp.prep_graph(G, relabel=relabel, del_self_loops=del_self_loops)
+
+    # Store preprocessed graph to a file
+    pp.save_graph(G, output_path=outpath + "prep_graph.edgelist", delimiter=' ', write_stats=False)
+
+    # Return the preprocessed graph
+    return G
+
+def preprocess1(G, outpath,  directed, relabel, del_self_loops):
+    """
+    Graph preprocessing routine.
+    """
+    print('Preprocessing graph...')
+
+    # Load a graph
+
+    # Preprocess the graph
+    G, ids = pp.prep_graph(G, relabel=relabel, del_self_loops=del_self_loops)
+
+    # Store preprocessed graph to a file
+    pp.save_graph(G, output_path=outpath + "prep_graph.edgelist", delimiter=' ', write_stats=False)
+
+    # Return the preprocessed graph
+    return G
+
+# from evalNE
+def eval_baselines(nee, directed, scoresheet):
+    """
+    Experiment to test the baselines.
+    """
+    print('Evaluating baselines...')
+
+    # Set the baselines
+    methods = ['common_neighbours', 'jaccard_coefficient', 'adamic_adar_index',
+               'preferential_attachment']
+
+    # Evaluate baseline methods
+    for method in methods:
+        if directed:
+            result = nee.evaluate_baseline(method=method, neighbourhood="in")
+            scoresheet.log_results(result)
+            result = nee.evaluate_baseline(method=method, neighbourhood="out")
+            scoresheet.log_results(result)
+        else:
+            result = nee.evaluate_baseline(method=method)
+            scoresheet.log_results(result)
+
+
+# from MNE
+def load_network_data(f_name):
+    # This function is used to load multiplex data
+    print('We are loading data from:', f_name)
+    edge_data_by_type = dict()
+    all_edges = list()
+    all_nodes = list()
+    with open(f_name, 'r') as f:
+        for line in f:
+            words = line[:-1].split(' ')
+            if words[0] not in edge_data_by_type:
+                edge_data_by_type[words[0]] = list()
+            edge_data_by_type[words[0]].append((words[1], words[2]))
+            all_edges.append((words[1], words[2]))
+            all_nodes.append(words[1])
+            all_nodes.append(words[2])
+    all_nodes = list(set(all_nodes))
+    # create common layer.
+    all_edges = list(set(all_edges))
+    edge_data_by_type['Base'] = all_edges
+    print('Finish loading data')
+    return edge_data_by_type, all_edges, all_nodes
+
+def get_scores(nee, res, results):
+    # Check the results
+    results = nee.get_results()
+#    for i in range (len(result_multiverse)):
+#        results.append(result_multiverse[i])
+    names = list()
+
+    for i in range(len(results)):
+
+        # Update the res variable with the results of the current repeat
+        if len(res) != len(results):
+            res.append(results[i].get_all())
+        else:
+            aux = results[i].get_all()
+            res[i] = (res[i][0], [res[i][1][k] + aux[1][k] for k in range(len(aux[1]))])
+
+        # Add the method names to a list
+        if 'edge_embed_method' in results[i].params:
+            names.append(results[i].method + '-' + results[i].params['edge_embed_method'])
+        else:
+            names.append(results[i].method)
+
+    return names, res
+
+
+     
+def netpreprocess(r_DistancematrixPPI, CLOSEST_NODES):
+    
+    # Number of nodes in the network and computation of neighborrhood
+    rawdata_DistancematrixPPI = np.array(r_DistancematrixPPI)
+    rawdata_DistancematrixPPI= np.transpose(rawdata_DistancematrixPPI)
+    node_size = np.shape(rawdata_DistancematrixPPI)[0]
+    neighborhood=[]
+    for i in range(node_size):
+        neighborhood.append(np.shape(np.extract(rawdata_DistancematrixPPI[i,:] > 1/node_size, rawdata_DistancematrixPPI[i,:]))[0])
+ 
+
+    # If several components
+    mini = []
+    rawdata_DistancematrixPPI = np.array(r_DistancematrixPPI)
+    
+    # change the diagonal and keep track of nodes of other components
+    component=[]
+    for i in range( 0,np.shape(rawdata_DistancematrixPPI)[0]):
+        mini.append(np.min(rawdata_DistancematrixPPI[i,:][np.nonzero(rawdata_DistancematrixPPI[i,:])]))
+        rawdata_DistancematrixPPI[i,i]= mini[i]
+        rawdata_DistancematrixPPI[i,component]=mini[i]
+    rawdata_DistancematrixPPI= np.transpose(rawdata_DistancematrixPPI)
+    
+    # Normalization 
+    rawdata_DistancematrixPPI  = normalize(rawdata_DistancematrixPPI, axis=1, norm='l1')
+    #data_DistancematrixPPI= rawdata_DistancematrixPPI
+    
+    
+    # Names of the nodes in the PPI network (a vocab in the sense of skipgram)
+    nodes = list(r_DistancematrixPPI.colnames)
+    nodes = [int(i) for i in nodes]
+    if nodes[0] == 1:
+        nodes = nodes - np.ones(len(nodes), dtype=int)
+    nodesstr = [str(i) for i in nodes]
+  
+    #DistancematrixPPI = pd.DataFrame(rawdata_DistancematrixPPI, nodesstr, nodesstr )
+    list_neighbours = []
+    reverse_data_DistancematrixPPI = []
+    for i in range(node_size):
+        sort_genes = rawdata_DistancematrixPPI[i,:]
+        sort_values = sorted(sort_genes, reverse=True)
+        sort_genes = np.argsort(-sort_genes)
+        
+        reverse_data_DistancematrixPPI.append(sort_values[0:CLOSEST_NODES])
+        list_neighbours.append(sort_genes)
+        
+    reverse_data_DistancematrixPPI=np.asarray(reverse_data_DistancematrixPPI)
+    list_neighbours = np.asarray(list_neighbours)
+    
+    
+    #reverse_data_DistancematrixPPI  = reverse_data_DistancematrixPPI[:,0:CLOSEST_NODES]
+    reverse_data_DistancematrixPPI  = normalize(reverse_data_DistancematrixPPI, axis=1, norm='l1')
+    
+    return reverse_data_DistancematrixPPI, list_neighbours, nodes, rawdata_DistancematrixPPI, neighborhood, nodesstr
+           
+   
+def netpreprocess1(r_DistancematrixPPI, CLOSEST_NODES):
+    
+    # Number of nodes in the network and computation of neighborrhood
+    rawdata_DistancematrixPPI = np.array(r_DistancematrixPPI)
+    rawdata_DistancematrixPPI= np.transpose(rawdata_DistancematrixPPI)
+    node_size = np.shape(rawdata_DistancematrixPPI)[0]
+    neighborhood=[]
+    for i in range(node_size):
+        neighborhood.append(np.shape(np.extract(rawdata_DistancematrixPPI[i,:] > 0, rawdata_DistancematrixPPI[i,:]))[0])
+ 
+
+    # If several components
+    mini = []
+    rawdata_DistancematrixPPI = np.array(r_DistancematrixPPI)
+    
+    # change the diagonal and keep track of nodes of other components
+    component=[]
+    for i in range( 0,np.shape(rawdata_DistancematrixPPI)[0]):
+        mini.append(np.min(rawdata_DistancematrixPPI[i,:]))
+        rawdata_DistancematrixPPI[i,i]= 1/25000
+        rawdata_DistancematrixPPI[i,component]=1/25000
+    rawdata_DistancematrixPPI= np.transpose(rawdata_DistancematrixPPI)
+    
+    # Normalization 
+    rawdata_DistancematrixPPI  = normalize(rawdata_DistancematrixPPI, axis=1, norm='l1')
+    #data_DistancematrixPPI= rawdata_DistancematrixPPI
+    
+    
+    # Names of the nodes in the PPI network (a vocab in the sense of skipgram)
+    nodes = list(r_DistancematrixPPI.index)
+    #nodes = [int(i) for i in nodes]
+    if nodes[0] == 1:
+        nodes = nodes - np.ones(len(nodes), dtype=int)
+    nodesstr = [str(i) for i in nodes]
+  
+    #DistancematrixPPI = pd.DataFrame(rawdata_DistancematrixPPI, nodesstr, nodesstr )
+    list_neighbours = []
+    reverse_data_DistancematrixPPI = []
+    for i in range(node_size):
+        sort_genes = rawdata_DistancematrixPPI[i,:]
+        sort_values = sorted(sort_genes, reverse=True)
+        sort_genes = np.argsort(-sort_genes)
+        
+        reverse_data_DistancematrixPPI.append(sort_values[0:CLOSEST_NODES])
+        list_neighbours.append(sort_genes)
+        
+    reverse_data_DistancematrixPPI=np.asarray(reverse_data_DistancematrixPPI)
+    list_neighbours = np.asarray(list_neighbours)
+    
+    
+    #reverse_data_DistancematrixPPI  = reverse_data_DistancematrixPPI[:,0:CLOSEST_NODES]
+    reverse_data_DistancematrixPPI  = normalize(reverse_data_DistancematrixPPI, axis=1, norm='l1')
+    
+    return reverse_data_DistancematrixPPI, list_neighbours, nodes, rawdata_DistancematrixPPI, neighborhood, nodesstr
+
+def loadGraphFromEdgeListTxt(graph, directed=True):
+    with open(graph, 'r') as g:
+        if directed:
+            G = nx.DiGraph()
+        else:
+            G = nx.Graph()
+        for lines in g:
+            edge = lines.strip().split()
+            if len(edge) == 3:
+                w = float(edge[2])
+            else:
+                w = 1.0
+            G.add_edge(int(edge[0]), int(edge[1]), weight=w)
+    return G
+    
+
+    
+        
